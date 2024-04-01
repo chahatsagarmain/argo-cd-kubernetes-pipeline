b7ed635543c70890c7383df094a9e7b01a6cc011
chahat sagar <chahatsagar2003@gmail.com>
2024-04-01 20:59:49 +0530

    fast api app with dockerfile

---
+++ app/src/controllers/controllers.py
@@ -0,0 +1,15 @@
+
+async def user_controller():
+    
+    print("controller for getting users")\
+    
+    users = { 1 : {
+        "name" : "chahat sagar",
+        "roll" : 1
+        },
+            2 : {
+        "name" : "second user",
+        "roll" : 2
+            }}
+    
+    return users
---
+++ app/src/routes/routes.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter , HTTPException
+from controllers.controllers import user_controller
+
+router = APIRouter()
+
+@router.get("/users")
+async def get_users():
+
+    query_set = await user_controller()
+    
+    return query_set
+
+@router.get("/users/{user_id}")
+async def get_user(user_id : int | None):
+    
+    if user_id is None:
+        return HTTPException(401 , { "message" : "query string missing"})
+    
+    query_set = await user_controller()
+    
+    if user_id in query_set:
+        return {"message" : "User present" , "user" : query_set[user_id]}
+    
+    return HTTPException(404 , {"message" : "user with user id not present"})
+
